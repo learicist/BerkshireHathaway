2f98518d82f12fe44f8ca527532d906044b15694
The Doctor <learicist@gmail.com>
2022-12-01 18:31:59 -0500

    addition of logic, cycling photo functionality, color scheme alterations

--- main.js
+++ main.js
@@ -4,4 +4,95 @@ $(document).ready(function () {
     $("#title").on("click", function () {
         location.reload();
     });
+
+    // CLOSE UPS
+    let images1 = [
+        "https://res.cloudinary.com/learicist/image/upload/v1669933413/BerkshireProject/solo.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929696/BerkshireProject/three.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929701/BerkshireProject/smile.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929542/BerkshireProject/closeup.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669061759/BerkshireProject/Pic1.jpg"
+    ];
+
+    // GROUP SHOTS
+    let images2 = [
+        "https://res.cloudinary.com/learicist/image/upload/v1669929631/BerkshireProject/group.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929608/BerkshireProject/group2.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929647/BerkshireProject/group3.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929601/BerkshireProject/group4.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929665/BerkshireProject/group5.jpg"
+    ];
+
+    // ACTION SHOTS
+    let images3 = [
+        "https://res.cloudinary.com/learicist/image/upload/v1669061764/BerkshireProject/DrumPic.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669933332/BerkshireProject/campfire.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669933386/BerkshireProject/arm.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929548/BerkshireProject/dance2.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929633/BerkshireProject/dance.jpg"
+    ];
+
+    // MISC SHOTS
+    let images4 = [
+        "https://res.cloudinary.com/learicist/image/upload/v1669929700/BerkshireProject/tiedye.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929575/BerkshireProject/food.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929649/BerkshireProject/guitar.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929686/BerkshireProject/hat2.jpg",
+        "https://res.cloudinary.com/learicist/image/upload/v1669929715/BerkshireProject/shirts.jpg"
+    ];
+
+    let index1 = 0,
+        index2 = 0,
+        index3 = 0,
+        index4 = 0;
+
+    let maxIndex = 4;
+
+    //PIC1 ROTATION
+    setInterval(function () {
+        let curr = images1[index1];
+        index1 = (maxIndex == index1) ? 0 : ++index1;
+        $('#pics1').fadeOut(350, function () {
+            $(this).attr("src", curr);
+            $(this).fadeIn(350);
+            //console.log(curr);
+        });
+        //console.log(images1[curr]);
+    }, 4000);
+
+    //PIC2 ROTATION
+    setInterval(function () {
+        let curr = images2[index2];
+        index2 = (maxIndex == index2) ? 0 : ++index2;
+        $('#pics2').fadeOut(350, function () {
+            $(this).attr("src", curr);
+            $(this).fadeIn(350);
+            //console.log(curr);
+        });
+        //console.log(images1[curr]);
+    }, 4000);
+
+    //PIC3 ROTATION
+    setInterval(function () {
+        let curr = images3[index3];
+        index3 = (maxIndex == index3) ? 0 : ++index3;
+        $('#pics3').fadeOut(350, function () {
+            $(this).attr("src", curr);
+            $(this).fadeIn(350);
+            //console.log(curr);
+        });
+        //console.log(images1[curr]);
+    }, 4000);
+
+    //PIC4 ROTATION
+    setInterval(function () {
+        let curr = images4[index4];
+        index4 = (maxIndex == index4) ? 0 : ++index4;
+        $('#pics4').fadeOut(350, function () {
+            $(this).attr("src", curr);
+            $(this).fadeIn(350);
+            //console.log(curr);
+        });
+        //console.log(images1[curr]);
+    }, 4000);
 });
